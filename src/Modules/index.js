15d14a8e929f1fbc0688622facebe83e450b5e8f
Yornan Garcia <jhornan.coningenieria@gmail.com>
2022-11-30 08:01:53 -0500

    index project array

---
+++ src/Modules/index.js
@@ -0,0 +1,11 @@
+import Jarillon from './Jarillon';
+import PozoRadial from './PozoRadial/index.js';
+import Users from './Users';
+
+ 
+ const Modules =  [Jarillon, PozoRadial,Users ]  
+
+  export default Modules
+
+ 
+ 

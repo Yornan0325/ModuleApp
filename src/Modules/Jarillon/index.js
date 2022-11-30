c071550dad3841285553cf93354ee894aa5303e0
Yornan Garcia <jhornan.coningenieria@gmail.com>
2022-11-30 10:18:25 -0500

    style list

--- src/Modules/Jarillon/index.js
+++ src/Modules/Jarillon/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Todo from "./Todo"
-import { useContext } from '../../Components/Context/Context';
+import { useContext } from '../../Context/Context';
 
 const Jarillon = () => {
     const { data, isLoading } = useContext();
-    console.log("data",data)
+console.log(data)
     return (
         <>
-            {data.map((item, i) => {
-                console.log("Iten", item)
-                return <Todo todo={item.title} key={i} isLoading={isLoading} />
-            })}
+            <div className="w-full max-w-full p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex items-center justify-between mb-4">
+                    <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+                    <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                        View all
+                    </a>
+                </div>
+                {data.map((item, i) => (
+                    <Todo todo={item} key={i} isLoading={isLoading} />
+
+                ))}
+            </div>
         </>
     )
 };
